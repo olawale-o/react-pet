620888261ff937dd642e1d4e27aecfaee5494f85
olawale-o <omoogunolawale@gmail.com>
2022-01-19 22:04:20 +0100

    Style view

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-const App = () => <div className="App">App Works</div>;
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Auth from './pages/User/Index';
+
+const App = () => (
+  <div className="App">
+    <Routes>
+      <Route path="/" element={<Auth />} />
+    </Routes>
+  </div>
+);
 
 export default App;
