ef15882d39cf43b811d7c4e013ba20012d7000f9
olawale-o <omoogunolawale@gmail.com>
2022-01-23 20:34:33 +0100

    Add style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/User/Home';
 import Auth from './pages/User/Index';
 
 const App = () => (
   <div className="App">
     <Routes>
       <Route path="/" element={<Auth />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   </div>
 );
