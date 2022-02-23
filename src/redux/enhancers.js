b3dbd0782b19875be4e2a9b937ef0c2d8ff3c790
olawale-o <omoogunolawale@gmail.com>
2022-02-23 09:51:54 +0100

    Create redux store enhancers

---
+++ src/redux/enhancers.js
@@ -0,0 +1,15 @@
+import { applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleWare from 'redux-thunk';
+import logger from './logger';
+
+const middlewares = [thunkMiddleWare];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+const middlewareEnhancers = applyMiddleware(...middlewares);
+
+const composedEnhancers = process.env.NODE_ENV === 'development' ? composeWithDevTools(middlewareEnhancers) : compose(middlewareEnhancers);
+
+export default composedEnhancers;
