212cbf30a0b83cee36a92b6e2cd0137a72e51856
olawale-o <omoogunolawale@gmail.com>
2022-02-23 10:28:23 +0100

    Intercept request and response

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { configureStore, persistor } from './redux/configureStore';
+import api from './api';
+
+const store = configureStore;
+
+api.interceptors.request.use((config) => {
+  const { auth: { token } } = store.getState();
+  const requestConfig = { ...config };
+  if (token) {
+    requestConfig.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use((response) => response, (error) => {
+  throw error;
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
