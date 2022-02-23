b9e43184ba8eade5064b6eeb9e24ef2fc4b26978
olawale-o <omoogunolawale@gmail.com>
2022-02-23 09:33:36 +0100

    Create routes

---
+++ src/components/routes/PrivateRoute.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import PropType from 'prop-types';
+import authSelector from '../../redux/auth/auth_selector';
+
+const PrivateRoute = ({ children }) => {
+  const { user } = useSelector(authSelector);
+  return user ? children : <Navigate to="/" />;
+};
+
+export default PrivateRoute;
+
+PrivateRoute.propTypes = {
+  children: PropType.node.isRequired,
+};
---
+++ src/components/routes/PublicRoute.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate, useLocation } from 'react-router-dom';
+import PropType from 'prop-types';
+import authSelector from '../../redux/auth/auth_selector';
+
+const PublicRoute = ({ children }) => {
+  const { user } = useSelector(authSelector);
+
+  const location = useLocation();
+  const from = location.state?.from?.pathname || '/';
+  if (user) return <Navigate to={from} />;
+  return children;
+};
+
+export default PublicRoute;
+
+PublicRoute.propTypes = {
+  children: PropType.node.isRequired,
+};
