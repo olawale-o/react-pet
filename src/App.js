dba268a4d2812cbf8681ce020a50fb571bf70b12
olawale-o <omoogunolawale@gmail.com>
2022-02-23 12:49:37 +0100

    Modify routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/User/Home';
 import Auth from './pages/User/Index';
+import { PublicRoute } from './components/routes';
+import { Navbar } from './components';
 
 const App = () => (
   <div className="App">
+    <Navbar />
     <Routes>
       <Route path="/" element={<Auth />} />
-      <Route path="/profile" element={<Home />} />
+      <Route path="/">
+        <Route index path="/" element={(<PublicRoute><Navigate to="listings" /></PublicRoute>)} />
+        <Route path="listings" element={(<PublicRoute><Home /></PublicRoute>)} />
+      </Route>
     </Routes>
   </div>
 );
