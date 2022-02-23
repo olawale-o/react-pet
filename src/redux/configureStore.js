8e1e05ba8b8e0a6b4720c990a5d4e6e285fde8f6
olawale-o <omoogunolawale@gmail.com>
2022-02-23 09:58:30 +0100

    Configure redux store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,20 @@
+
+import { createStore, combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import authReducer from './auth/auth';
+import composedEnhancers from './enhancers';
+import globalReducer from './global/global';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const reducer = combineReducers({ global: globalReducer, auth: authReducer });
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const configureStore = createStore(persistedReducer, composedEnhancers);
+const persistor = persistStore(configureStore);
+
+export { configureStore, persistor };
