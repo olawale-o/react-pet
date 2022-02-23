cbe82b0f551c025a1be94d8d279702bd4a7cf87f
olawale-o <omoogunolawale@gmail.com>
2022-02-23 09:28:31 +0100

    Modify css

--- src/components/PetListing/index.js
+++ src/components/PetListing/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import ListingPetCard from '../ListingPetCard';
+import Filter from '../Filter';
 import './style.scss';
 
 const PetListing = () => {
   console.log('ListingPet');
 
   return (
-    <div className="listing__pet">
-      <ListingPetCard />
-      <ListingPetCard />
-      <ListingPetCard />
-      <ListingPetCard />
-      <ListingPetCard />
-      <ListingPetCard />
-      <ListingPetCard />
-      <ListingPetCard />
-      <ListingPetCard />
-      <ListingPetCard />
-      <ListingPetCard />
+    <div className="listings">
+      <Filter />
+      <div className="listing__pet">
+        <ListingPetCard />
+        <ListingPetCard />
+        <ListingPetCard />
+        <ListingPetCard />
+        <ListingPetCard />
+        <ListingPetCard />
+        <ListingPetCard />
+        <ListingPetCard />
+        <ListingPetCard />
+        <ListingPetCard />
+        <ListingPetCard />
+      </div>
     </div>
   );
 };
