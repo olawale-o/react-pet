ef429aa9baed1c6b91c4fe264f366c58d1c88048
olawale-o <omoogunolawale@gmail.com>
2022-03-06 21:42:54 +0100

    Hide button on user state

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,19 +1,31 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import authSelector from '../../redux/auth/auth_selector';
 import './style.scss';
 
-const Navbar = () => (
-  <header className="header">
-    <nav className="nav">
-      <div className="logo">
-        <Link to="/">Paws</Link>
-      </div>
-      <ul className="nav__list">
-        <li className="nav__item">
-          <Link to="/" type="button" className="btn__link btn-login">Login</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+const Navbar = () => {
+  const { user } = useSelector(authSelector);
+  return (
+    <header className="header">
+      <nav className="nav">
+        <div className="logo">
+          <Link to="/">Paws</Link>
+        </div>
+        <ul className="nav__list">
+          {!user && (
+            <li className="nav__item">
+              <Link to="/" type="button" className="btn__link btn-login">Login</Link>
+            </li>
+          )}
+          {user && (
+            <li className="nav__item">
+              <Link to="/" type="button" className="btn__link btn-login">Logout</Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
