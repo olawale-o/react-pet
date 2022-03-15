772334ebd8e86d346944b2822dd08eaaa98d59b8
olawale-o <omoogunolawale@gmail.com>
2022-03-15 17:29:22 +0100

    Add petReducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import authReducer from './auth';
+import petReducer from './pet';
 import composedEnhancers from './enhancers';
 import globalReducer from './global';
 
@@ -10,7 +11,11 @@ const persistConfig = {
   storage,
 };
 
-const reducer = combineReducers({ global: globalReducer, auth: authReducer });
+const reducer = combineReducers({
+  global: globalReducer,
+  auth: authReducer,
+  pet: petReducer,
+});
 const persistedReducer = persistReducer(persistConfig, reducer);
 
 const configureStore = createStore(persistedReducer, composedEnhancers);
