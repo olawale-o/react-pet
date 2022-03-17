770f59a4f7a7a2c4a71d4356aaee93a2d07504b9
olawale-o <omoogunolawale@gmail.com>
2022-03-17 14:02:07 +0100

    Add breed and color fields to pet form

--- src/components/Pet/NewPetForm/PetForm.js
+++ src/components/Pet/NewPetForm/PetForm.js
@@ -1,16 +1,62 @@
 import React from 'react';
 import PropType from 'prop-types';
-import { CustomInput, CustomSlider } from '../../../forms';
+import {
+  CustomInput,
+  CustomSlider,
+  CustomRadioGroup,
+  CustomTextArea,
+  CustomAutoSuggest,
+} from '../../../forms';
+import petColors from '../../../constants/pet_colors';
 
 const PetForm = ({ formField }) => {
   const rangeValueElement = React.useRef();
-  const { petName, petWeight, petGender } = formField;
+  const [pawColors, setPawColors] = React.useState(petColors);
+  const searchPetColorRef = React.useRef();
+  const searchPetBreedRef = React.useRef();
+  const [isPetColorListVisible, setIsPetColorListVisible] = React.useState(false);
+  const [isPetBreedListVisible, setIsetBreedListVisible] = React.useState(false);
+
+  const {
+    petName,
+    petWeight,
+    petGender,
+    petColor,
+    petDescription,
+    petBreed,
+  } = formField;
   const onSliderMove = (value) => {
     rangeValueElement.current.style.left = `${value / 2}%`;
     rangeValueElement.current.classList.add('show');
   };
 
   const onSlideBlur = () => rangeValueElement.current.classList.remove('show');
+
+  const onSearch = (value) => {
+    if (value.trim() === '') {
+      setPawColors(petColors);
+    } else {
+      const filterColors = petColors.filter((color) => color.startsWith(value.toLowerCase()));
+      if (filterColors.length > 0) {
+        setPawColors(filterColors);
+      } else {
+        setPawColors([value]);
+      }
+    }
+  };
+
+  React.useEffect(() => {
+    const onOutSideClick = (event) => {
+      if (searchPetColorRef.current && !searchPetColorRef.current.contains(event.target)) {
+        setIsPetColorListVisible(false);
+      }
+      if (searchPetBreedRef.current && !searchPetBreedRef.current.contains(event.target)) {
+        setIsetBreedListVisible(false);
+      }
+    };
+    document.addEventListener('click', onOutSideClick);
+    return () => document.removeEventListener('click', onOutSideClick);
+  }, []);
 
   return (
     <div>
@@ -28,17 +74,61 @@ const PetForm = ({ formField }) => {
         />
       </div>
       <div className="field">
+        <CustomAutoSuggest
+          type="text"
+          name={petColor.name}
+          autoComplete="off"
+          placeholder="Pet Color"
+          onSearch={onSearch}
+          onSelected={() => setIsPetColorListVisible(false)}
+          list={pawColors}
+          el={searchPetColorRef}
+          onFocus={() => setIsPetColorListVisible(true)}
+          isVisible={isPetColorListVisible}
+        />
+      </div>
+      <div className="field">
+        <CustomAutoSuggest
+          type="text"
+          name={petBreed.name}
+          autoComplete="off"
+          placeholder="Pet Breed"
+          onSearch={onSearch}
+          onSelected={() => setIsetBreedListVisible(false)}
+          list={pawColors}
+          el={searchPetBreedRef}
+          onFocus={() => setIsetBreedListVisible(true)}
+          isVisible={isPetBreedListVisible}
+        />
+      </div>
+      <div className="field">
         <span className="label">Gender</span>
-        <CustomInput type="radio" name={petGender.name} value="1" />
-        <span className="radio__label">Male</span>
-        <CustomInput type="radio" name={petGender.name} value="2" />
-        <span className="radio__label">Female</span>
+        <CustomRadioGroup
+          name={petGender.name}
+          fields={[
+            {
+              label: 'Male',
+              fieldValue: 'm',
+            },
+            {
+              label: 'Female',
+              fieldValue: 'f',
+            },
+          ]}
+        />
+      </div>
+      <div className="field">
+        <CustomTextArea name={petDescription.name} placeholder="Description" />
       </div>
     </div>
   );
 };
 
 export default PetForm;
+
+PetForm.defaultProps = {
+  errors: {},
+};
 
 PetForm.propTypes = {
   formField: PropType.shape({
@@ -51,5 +141,15 @@ PetForm.propTypes = {
     petGender: PropType.shape({
       name: PropType.string.isRequired,
     }).isRequired,
+    petColor: PropType.shape({
+      name: PropType.string.isRequired,
+    }).isRequired,
+    petDescription: PropType.shape({
+      name: PropType.string.isRequired,
+    }).isRequired,
+    petBreed: PropType.shape({
+      name: PropType.string.isRequired,
+    }).isRequired,
   }).isRequired,
+  errors: PropType.shape({}),
 };
--- src/components/Pet/NewPetForm/index.js
+++ src/components/Pet/NewPetForm/index.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
 import { Formik, Form } from 'formik';
+import PropType from 'prop-types';
 import './style.scss';
 import { newPetInitialValues, newPetSchema, newPetModel } from '../../../forms';
+import createPet from '../../../redux/pet/pet_async_action';
 import PetForm from './PetForm';
+import { createDogService } from '../../../services';
 
 const { formField } = newPetModel;
 
@@ -9,21 +14,39 @@ const renderForm = () => (
   <PetForm formField={formField} />
 );
 
-const NewPetForm = () => {
-  console.log('new pet form');
+const NewPetForm = ({ onSubmit }) => {
+  const handleSubmit = async ({
+    petName,
+    petWeight,
+    petColor,
+    petGender,
+  }) => {
+    await onSubmit({
+      dog: {
+        name: petName,
+        weight: petWeight,
+        color: petColor,
+        gender: petGender,
+      },
+    }, createDogService);
+  };
+
   return (
     <div className="pet__form">
       <Formik
         initialValues={newPetInitialValues}
         validationSchema={newPetSchema}
-        handleSubmit={() => {}}
+        onSubmit={handleSubmit}
       >
-        {() => (
+        {({ values }) => (
           <Form>
             {renderForm()}
             <div className="actions">
               <button type="submit" className="btn btn--primary">Next</button>
             </div>
+            <pre>
+              {JSON.stringify(values, null, 2)}
+            </pre>
           </Form>
         )}
       </Formik>
@@ -31,4 +54,12 @@ const NewPetForm = () => {
   );
 };
 
-export default NewPetForm;
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit: (data, service, push) => dispatch(createPet(data, service, push)),
+});
+
+NewPetForm.propTypes = {
+  onSubmit: PropType.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(NewPetForm);
