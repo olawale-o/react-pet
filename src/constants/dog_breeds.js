4b4965568d57e149810835d8f0bf1417857a1440
olawale-o <omoogunolawale@gmail.com>
2022-03-21 14:51:19 +0100

    Export dog breeds

--- src/constants/dog_breeds.js
+++ src/constants/dog_breeds.js
@@ -1,5 +1,5 @@
 const DOG_BREEDS = [
-  'afador','affenhuahua','affenpinscher', 'afghan hound', 'airedale terrier',
+  'afador', 'affenhuahua', 'affenpinscher', 'afghan hound', 'airedale terrier',
   'akbash', 'akita', 'akita chow', 'akita pit', 'akita shepherd', 'alaskan malamute',
   'alaskan klee kai', 'alaskan husky', 'american bulldog', 'american english coonhound',
   'american eskimo dog', 'american foxhound', 'american hairless terrier',
@@ -9,10 +9,12 @@ const DOG_BREEDS = [
   'australian cattle dog', 'australian kelpie', 'australian retriever', 'australian shepherd',
   'australian shepherd husky', 'australian shepherd lab mix', 'australian shepherd pit bull mix',
   'australian stumpy tail cattle dog', 'australian terrier', 'azawakh',
-  'babvet', 'basenji', 'bassador', 'basset fauve de bretagne','basset hound',
+  'babvet', 'basenji', 'bassador', 'basset fauve de bretagne', 'basset hound',
   'basset retriever', 'bavarian mountain scent hound', 'beabull', 'beagle',
   'beaglier', 'bearded collie', 'bedlington terrier', 'belgian laekenois', 'belgian malinois',
   'belgian sheepdog', 'belgian tervuren', 'Bergamasco Sheepdog', 'Berger Picard',
   'Bernedoodle', 'Bernese Mountain Dog', 'Bichon Frise', 'Biewer Terrier', 'Black and Tan Coonhound',
   'Black Mouth Cur', 'Black Russian Terrier', 'Bloodhound', 'Blue Lacy',
 ];
+
+export default DOG_BREEDS;
