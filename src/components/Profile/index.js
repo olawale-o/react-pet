517c31e52f9733fedba19d1f0e5e01204cff4a66
olawale-o <omoogunolawale@gmail.com>
2022-03-23 10:26:35 +0100

    Pass props to ProfileArea component

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
+import PropType from 'prop-types';
 import './style.scss';
 import MyPetCard from '../MyPetCard';
 
-const ProfileArea = () => {
+const ProfileArea = ({ myPets }) => {
   console.log('ProfileArea');
   return (
     <div className="profile__area">
       <div className="pets">
-        <MyPetCard pet={{
-          name: 'Luna',
-          offerCount: 2,
-          likeCount: 5,
-        }}
-        />
+        {myPets.map((pet) => (
+          <MyPetCard
+            key={pet.id}
+            pet={{
+              name: pet.name,
+              offerCount: 2,
+              likeCount: 5,
+              color: pet.color,
+              gender: pet.gender,
+            }}
+          />
+        ))}
       </div>
     </div>
   );
 };
 
 export default ProfileArea;
+
+ProfileArea.propTypes = {
+  myPets: PropType.arrayOf(PropType.shape({
+    name: PropType.string,
+    gender: PropType.string,
+    color: PropType.string,
+  })).isRequired,
+};
