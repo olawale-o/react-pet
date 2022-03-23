4d4598f7312300127fb6f01b32df98035d981de1
olawale-o <omoogunolawale@gmail.com>
2022-03-23 10:25:35 +0100

    Fetch authorized users pets

--- src/pages/Pet/All/index.js
+++ src/pages/Pet/All/index.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { ProfileArea } from '../../../components';
+import { getAllPets } from '../../../redux/pet/pet_async_action';
+import { allDogService } from '../../../services';
+import petSelector from '../../../redux/pet/pet_selector';
 
-const MyPets = () => <ProfileArea />;
+const MyPets = () => {
+  const dispatch = useDispatch();
+  const { userId } = useParams();
+  const { myPets } = useSelector(petSelector);
+
+  React.useEffect(() => {
+    dispatch(getAllPets(allDogService));
+  }, [userId]);
+  return (<ProfileArea myPets={myPets} />);
+};
 
 export default MyPets;
