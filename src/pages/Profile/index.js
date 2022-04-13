2556d1f4ff7235bcbe65865a33002b6473dbbaa6
olawale-o <omoogunolawale@gmail.com>
2022-04-13 13:37:55 +0100

    Map state to props

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Outlet } from 'react-router-dom';
 import { ProfileSideBar } from '../../components';
 import './style.scss';
 
-const Profile = () => {
+const Profile = ({ user }) => {
   console.log('Profile');
   return (
     <div className="profile">
       <div className="content">
         <div className="main">
-          <ProfileSideBar />
+          <ProfileSideBar userId={user.id} />
           <div className="outlet">
             <Outlet />
           </div>
@@ -19,4 +21,14 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+const mapStateToProps = (state) => ({
+  user: state.auth.user,
+});
+
+export default connect(mapStateToProps, null)(Profile);
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
