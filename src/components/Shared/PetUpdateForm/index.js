42b79e945f0f699d62fee0b7f8d427b87a9f23f1
olawale-o <omoogunolawale@gmail.com>
2022-04-21 02:02:21 +0100

    Fix props

--- src/components/Shared/PetUpdateForm/index.js
+++ src/components/Shared/PetUpdateForm/index.js
@@ -1,14 +1,23 @@
+import PropType from 'prop-types';
 import './style.scss';
 
-const PetUpdateForm = () => {
-  console.log('update form');
+const PetUpdateForm = ({ selectedPetIndex, el }) => {
+  if (!selectedPetIndex) {
+    return null;
+  }
   return (
     <div className="modal">
-      <div className="modal__body">
+      <div className="modal__body" ref={el}>
         Modal
+        {selectedPetIndex}
       </div>
     </div>
   );
 };
 
 export default PetUpdateForm;
+
+PetUpdateForm.propTypes = {
+  selectedPetIndex: PropType.bool.isRequired,
+  el: PropType.shape({ current: PropType.instanceOf(Element) }).isRequired,
+};
