1bdb74a26886ba4abf675dddb0b9609cb4250a72
olawale-o <omoogunolawale@gmail.com>
2022-04-24 18:01:22 +0100

    Fetch selected pet for update

--- src/components/Pet/UpdatePetForm/index.js
+++ src/components/Pet/UpdatePetForm/index.js
@@ -1,23 +1,71 @@
-import PropType from 'prop-types';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
+import { connect } from 'react-redux';
+import PropType from 'prop-types';
+import { getSelectedPetService } from '../../../services/pet';
+import { getSelectedPet } from '../../../redux/pet/pet_async_action';
 
-const UpdatePetForm = ({ selectedPetIndex, el }) => {
-  if (!selectedPetIndex) {
+const UpdatePetForm = ({
+  id,
+  pet,
+  fetchSelectePet,
+  userId,
+  open,
+  closePopUp,
+  loading,
+}) => {
+  if (!open) {
     return null;
   }
+  console.log(pet);
+  React.useEffect(() => {
+    if (id) {
+      fetchSelectePet({ petId: id, userId }, getSelectedPetService);
+    }
+  }, [id]);
   return (
-    <div className="modal">
-      <div className="modal__body" ref={el}>
-        Modal
-        {selectedPetIndex}
-      </div>
-    </div>
+    ReactDOM.createPortal(
+      <>
+        <div className="modal" onClick={closePopUp} aria-hidden="true" />
+        {loading && <div className="loading">Loading...</div>}
+        {!loading
+          && (
+            <div className="modal__body">
+              Modal
+              {id}
+            </div>
+          )}
+      </>,
+      document.body,
+    )
   );
 };
 
-export default UpdatePetForm;
+const mapStateToProps = (state) => ({
+  pet: state.pet.selectedPet,
+  userId: state.auth.user.id,
+  loading: state.global.loading,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchSelectePet: (credential, service) => dispatch(getSelectedPet(credential, service)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(UpdatePetForm);
+
+UpdatePetForm.defaultProps = {
+  pet: {},
+};
 
 UpdatePetForm.propTypes = {
-  selectedPetIndex: PropType.oneOfType([PropType.bool, PropType.number]).isRequired,
-  el: PropType.shape({ current: PropType.instanceOf(Element) }).isRequired,
+  closePopUp: PropType.func.isRequired,
+  open: PropType.bool.isRequired,
+  id: PropType.number.isRequired,
+  userId: PropType.number.isRequired,
+  pet: PropType.shape({
+    id: PropType.number,
+  }),
+  fetchSelectePet: PropType.func.isRequired,
+  loading: PropType.bool.isRequired,
 };
