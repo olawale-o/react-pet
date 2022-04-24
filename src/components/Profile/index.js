abacfd92731814df91a4533200a0edd7d3672ff6
olawale-o <omoogunolawale@gmail.com>
2022-04-24 17:53:47 +0100

    Show modal when pet selected

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -2,26 +2,61 @@ import React from 'react';
 import PropType from 'prop-types';
 import './style.scss';
 import MyPetCard from '../MyPetCard';
+import UpdatePetForm from '../Pet/UpdatePetForm';
 
-const ProfileArea = ({ myPets }) => (
-  <div className="profile__area">
-    <div className="pets">
-      {myPets.map((pet) => (
-        <MyPetCard
-          key={pet.id}
-          pet={{
-            id: pet.id,
-            name: pet.name,
-            offerCount: 2,
-            likeCount: 5,
-            color: pet.color,
-            gender: pet.gender,
-          }}
-        />
-      ))}
+const ProfileArea = ({ myPets }) => {
+  const [choosenPet, setChoosenPet] = React.useState(null);
+  const [overlayOpen, toggleOverlayOpen] = React.useState(false);
+  const [modalOpen, toggleModalOpen] = React.useState(false);
+  const [index, setIndex] = React.useState(null);
+  const onChoosePet = (petId) => {
+    setChoosenPet(petId);
+    setIndex(petId);
+    toggleOverlayOpen(!overlayOpen);
+  };
+  const close = () => {
+    toggleOverlayOpen(!overlayOpen);
+    setChoosenPet(null);
+  };
+  const openModal = () => {
+    toggleModalOpen(!modalOpen);
+    toggleOverlayOpen(!overlayOpen);
+    setChoosenPet(null);
+  };
+  return (
+    <div className="profile__area">
+      <div className="pets">
+        {myPets.map((pet) => (
+          <MyPetCard
+            key={pet.id}
+            choosenPet={choosenPet}
+            setChoosenPet={setChoosenPet}
+            onChoosePet={onChoosePet}
+            openModal={openModal}
+            modal={modalOpen}
+            pet={{
+              id: pet.id,
+              name: pet.name,
+              offerCount: 2,
+              likeCount: 5,
+              color: pet.color,
+              gender: pet.gender,
+            }}
+          />
+        ))}
+      </div>
+      {overlayOpen
+        && (
+          <div className="overlay" aria-hidden="true" onClick={close} />
+        )}
+      <UpdatePetForm
+        id={index}
+        open={modalOpen}
+        closePopUp={() => toggleModalOpen(!modalOpen)}
+      />
     </div>
-  </div>
-);
+  );
+};
 
 export default ProfileArea;
 
