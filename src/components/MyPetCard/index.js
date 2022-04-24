2a6828f0cbfb1bc50e83473d1c8316d1a0046c2f
olawale-o <omoogunolawale@gmail.com>
2022-04-24 13:46:19 +0100

    Detect click outside dom to close tooltip

--- src/components/MyPetCard/index.js
+++ src/components/MyPetCard/index.js
@@ -3,90 +3,63 @@ import { AiOutlineHeart, AiOutlineEllipsis } from 'react-icons/ai';
 import { MdOutlineLocalOffer } from 'react-icons/md';
 import PropType from 'prop-types';
 import secondDog from '../../assets/images/dog2.jpg';
-import { titlelize, GENDER_ENUM, useNavigator } from '../../helper';
+import { titlelize, GENDER_ENUM } from '../../helper';
+import PetToolTipPopUp from '../Shared/PetToolTipPopUp';
 import './style.scss';
-import UpdatePetForm from '../Pet/UpdatePetForm';
-import usePopUp from '../../composables';
 
 const MyPetCard = ({
-  pet: {
-    id,
-    name,
-    offerCount,
-    likeCount,
-    gender,
-    color,
-  },
+  pet,
+  choosenPet,
+  onChoosePet,
+  openModal,
+  modal,
 }) => {
-  const { pushAndReplace } = useNavigator();
-  const ref = React.useRef();
-  const modalPetRef = React.useRef();
-  const card = usePopUp(ref, false);
-  const modal = usePopUp(modalPetRef, 0);
+  console.log('sungba');
   return (
-    <>
-      <div className="pet__card">
-        {card.isVisible
-          && (
-          <ul className="action__card" ref={ref}>
-            <li className="action__card-item">
-              <button
-                type="button"
-                className="action__btn"
-                onClick={() => pushAndReplace('/listings')}
-              >
-                Delete
-              </button>
-            </li>
-            <li className="action__card-item">
-              <button
-                type="button"
-                className="action__btn"
-                onClick={() => modal.setIsVisible(id)}
-              >
-                Edit
-              </button>
-            </li>
-          </ul>
-          )}
-        <div className="pet__image">
-          <button type="button" className="remove__btn" onClick={() => card.setIsVisible(!card.isVisible)}>
-            <span><AiOutlineEllipsis size={30} color="#fff" /></span>
-          </button>
-          <img src={secondDog} alt="dog" />
-        </div>
-        <div className="pet__content">
-          <h6 className="name">{titlelize(name)}</h6>
-          <div className="info">
-            <span>{GENDER_ENUM[gender]}</span>
-            <span>{titlelize(color)}</span>
-          </div>
-          <div className="interactions">
-            <button type="button" className="interaction__btn">
-              <span>
-                <AiOutlineHeart size={20} />
-              </span>
-              <span>{likeCount}</span>
-            </button>
-            <button type="button" className="interaction__btn">
-              <span>
-                <MdOutlineLocalOffer />
-              </span>
-              <span>{offerCount}</span>
-            </button>
-          </div>
-          <button type="button" className="details">Details</button>
-        </div>
+    <div className="pet__card">
+      {choosenPet === pet.id && !modal
+        && (
+          <PetToolTipPopUp
+            action={() => openModal()}
+          />
+        )}
+      <div className="pet__image">
+        <button type="button" className="remove__btn" onClick={() => onChoosePet(pet.id)}>
+          <span><AiOutlineEllipsis size={30} color="#fff" /></span>
+        </button>
+        <img src={secondDog} alt="dog" />
       </div>
-      <UpdatePetForm
-        el={modalPetRef}
-        selectedPetIndex={modal.isVisible}
-      />
-    </>
+      <div className="pet__content">
+        <h6 className="name">{titlelize(pet.name)}</h6>
+        <div className="info">
+          <span>{GENDER_ENUM[pet.gender]}</span>
+          <span>{titlelize(pet.color)}</span>
+        </div>
+        <div className="interactions">
+          <button type="button" className="interaction__btn">
+            <span>
+              <AiOutlineHeart size={20} />
+            </span>
+            <span>{pet.likeCount}</span>
+          </button>
+          <button type="button" className="interaction__btn">
+            <span>
+              <MdOutlineLocalOffer />
+            </span>
+            <span>{pet.offerCount}</span>
+          </button>
+        </div>
+        <button type="button" className="details">Details</button>
+      </div>
+    </div>
   );
 };
 
 export default MyPetCard;
+
+MyPetCard.defaultProps = {
+  choosenPet: null,
+};
 
 MyPetCard.propTypes = {
   pet: PropType.shape({
@@ -97,4 +70,8 @@ MyPetCard.propTypes = {
     color: PropType.string,
     gender: PropType.string,
   }).isRequired,
+  choosenPet: PropType.number,
+  onChoosePet: PropType.func.isRequired,
+  openModal: PropType.func.isRequired,
+  modal: PropType.bool.isRequired,
 };
