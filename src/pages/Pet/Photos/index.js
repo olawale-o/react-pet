50017af69077eb0c1ef8e44a3632e07cfca6a869
olawale-o <omoogunolawale@gmail.com>
2022-05-06 15:55:19 +0100

    Fetch selected pet photos

--- src/pages/Pet/Photos/index.js
+++ src/pages/Pet/Photos/index.js
@@ -1,5 +1,24 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import PhotoList from '../../../components/Pet/PhotoList';
+import { getPetPhotosService } from '../../../services';
+import { getPetPhotos } from '../../../redux/pet/pet_async_action';
+import petSelector from '../../../redux/pet/pet_selector';
 
-const Photos = () => <PhotoList />;
+const Photos = () => {
+  const dispatch = useDispatch();
+  const { petId } = useParams();
+  const { photos } = useSelector(petSelector);
+  React.useEffect(() => {
+    dispatch(getPetPhotos(getPetPhotosService, { petId }));
+  }, [petId]);
+
+  if (!photos) return null;
+
+  return (
+    <PhotoList photos={photos} />
+  );
+};
 
 export default Photos;
