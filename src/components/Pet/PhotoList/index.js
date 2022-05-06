f8c0a49da9481f46e7fc296783d4d0c8aa5028da
olawale-o <omoogunolawale@gmail.com>
2022-05-06 15:55:47 +0100

    Display pet photo

--- src/components/Pet/PhotoList/index.js
+++ src/components/Pet/PhotoList/index.js
@@ -1,5 +1,25 @@
 import './style.scss';
+import PropType from 'prop-types';
+import { HiPencil } from 'react-icons/hi';
+import BASE_URI from '../../../constants';
 
-const PhotoList = () => <div>Phot works</div>;
+const PhotoList = ({ photos }) => (
+  <div className="photo__list">
+    {photos.map((photo) => (
+      <div className="photo" key={photo.url}>
+        <img src={`${BASE_URI}${photo.url}`} alt="dog" />
+        <button type="button" className="photo__btn" onClick={() => {}}>
+          <span><HiPencil size={30} color="#fff" /></span>
+        </button>
+      </div>
+    ))}
+  </div>
+);
 
 export default PhotoList;
+
+PhotoList.propTypes = {
+  photos: PropType.arrayOf(PropType.shape({
+    url: PropType.string,
+  })).isRequired,
+};
