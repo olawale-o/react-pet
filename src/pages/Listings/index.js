2aeb1e1c9770c7ea93afbf7b6ca1572cd7cad322
olawale-o <omoogunolawale@gmail.com>
2022-05-08 09:04:38 +0100

    Fetch all pets

--- src/pages/Listings/index.js
+++ src/pages/Listings/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { PetListing, SideSearchPanel } from '../../components';
+import { getAllPets } from '../../redux/pet/pet_async_action';
+import { allDogService } from '../../services';
 
-const Listings = () => (
-  <>
-    <SideSearchPanel />
-    <PetListing />
-  </>
-);
+const Listings = ({ pets, fetchPets }) => {
+  React.useEffect(() => {
+    fetchPets(allDogService);
+  }, []);
+  if (!pets) return null;
+  return (
+    <>
+      <SideSearchPanel />
+      <PetListing pets={pets} />
+    </>
+  );
+};
 
-export default Listings;
+const mapStateToProps = (state) => ({
+  pets: state.pet.allPets,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchPets: (service) => dispatch(getAllPets(service)),
+});
+
+Listings.propTypes = {
+  pets: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+  })).isRequired,
+  fetchPets: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Listings);
