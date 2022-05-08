3971dd1268a51f29b5502ab0147c1894c50bb449
olawale-o <omoogunolawale@gmail.com>
2022-05-08 09:05:38 +0100

    Display all pets

--- src/components/PetListing/index.js
+++ src/components/PetListing/index.js
@@ -1,29 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ListingPetCard from '../ListingPetCard';
 import Filter from '../Filter';
 import './style.scss';
 
-const PetListing = () => {
+const PetListing = ({ pets }) => {
   console.log('ListingPet');
 
   return (
     <div className="listings">
       <Filter />
       <div className="listing__pet">
-        <ListingPetCard />
-        <ListingPetCard />
-        <ListingPetCard />
-        <ListingPetCard />
-        <ListingPetCard />
-        <ListingPetCard />
-        <ListingPetCard />
-        <ListingPetCard />
-        <ListingPetCard />
-        <ListingPetCard />
-        <ListingPetCard />
+        {pets.map((pet) => (
+          <ListingPetCard key={pet.id} pet={pet} />
+        ))}
       </div>
     </div>
   );
 };
 
+PetListing.propTypes = {
+  pets: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+  })).isRequired,
+};
 export default PetListing;
