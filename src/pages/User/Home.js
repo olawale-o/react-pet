efec38a375e7ec5bca5957a384f9d7cec383177d
olawale-o <omoogunolawale@gmail.com>
2022-05-23 07:47:15 +0100

    Paginate an filter search by gender

--- src/pages/User/Home.js
+++ src/pages/User/Home.js
@@ -1,27 +1,84 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import HomeContext from '../../context/HomeContext';
 import Listings from '../Listings';
 import './Home.scss';
 import { allDogService } from '../../services';
 import { getAllPets } from '../../redux/pet/pet_async_action';
+import { genders } from '../../constants';
 
 const Home = ({ petIds, fetchPets, searchMeta }) => {
-  const [currentPage, setCurrentPage] = React.useState(1);
-  const { prev_page: prevPage, next_page: nextPage } = searchMeta;
-  const paginate = async (page) => {
-    await fetchPets(allDogService, { page });
+  const [search, setSearch] = useSearchParams();
+  const [gender, setGender] = React.useState('b');
+  const [breed, setBreed] = React.useState('both');
+  const [pawColor, setPawColor] = React.useState('all');
+  const [currentPage, setCurrentPage] = React.useState(0);
+  const [direction, setDirection] = React.useState('next');
+  const {
+    prev_page_no: prevPageNo,
+    next_page_no: nextPageNo,
+  } = searchMeta;
+
+  const paginate = (page, direction) => {
+    const gender = search.get('gender');
+    fetchPets(allDogService, { page, gender, direction });
     setCurrentPage(page);
+    setDirection(direction);
   };
+  const onGenderSelected = (e) => {
+    const { target: { value } } = e;
+    setGender(value);
+    setSearch({ gender: value, breeder: breed, color: pawColor });
+    fetchPets(allDogService, { page: currentPage, gender: value, direction });
+  };
+
+  const onBreedSelected = (e) => {
+    const { target: { value } } = e;
+    setBreed(value);
+    setSearch({
+      gender,
+      breeder: value,
+      color: pawColor,
+      direction,
+    });
+  };
+
+  const onColorSelected = (value) => {
+    setSearch({
+      gender,
+      breeder: breed,
+      color: value,
+      direction,
+    });
+  };
+
   React.useEffect(() => {
-    fetchPets(allDogService, { page: currentPage });
+    setSearch({
+      gender: 'b',
+      breeder: 'b',
+      color: 'all',
+    });
+    fetchPets(allDogService,
+      {
+        page: currentPage,
+        breeder: 'b',
+        color: 'all',
+        direction: 'next',
+      });
   }, []);
   if (!petIds) return null;
   return (
     <HomeContext.Provider value={{
-      prevPage,
-      nextPage,
+      genders,
+      gender,
+      onGenderSelected,
+      breed,
+      onBreedSelected,
+      pawColor,
+      prevPageNo,
+      nextPageNo,
       paginate,
       petIds,
     }}
@@ -29,7 +86,10 @@ const Home = ({ petIds, fetchPets, searchMeta }) => {
       <div className="home">
         <div className="content">
           <div className="main">
-            <Listings />
+            <Listings
+              setPawColor={setPawColor}
+              onColorSelected={onColorSelected}
+            />
           </div>
         </div>
       </div>
@@ -43,7 +103,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchPets: (service, page) => dispatch(getAllPets(service, page)),
+  fetchPets: (service, credential) => dispatch(getAllPets(service, credential)),
 });
 
 Home.defaultProps = {
@@ -56,8 +116,10 @@ Home.propTypes = {
   searchMeta: PropTypes.shape({
     total_items: PropTypes.number,
     current_page: PropTypes.number,
-    next_page: PropTypes.number,
-    prev_page: PropTypes.number,
+    next_page_no: PropTypes.number,
+    prev_page_no: PropTypes.number,
+    total_current_items: PropTypes.number,
+    is_last_item: PropTypes.bool,
   }).isRequired,
 };
 
